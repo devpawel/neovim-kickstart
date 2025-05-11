f5bd9705e4ce28c8ea186fe208ecee121fbd7d73
Pawel <23643172+devpawel@users.noreply.github.com>
2025-05-11 14:05:12 +0200

    Add Diffview

---
+++ lua/custom/plugins/diffview.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    'sindrets/diffview.nvim',
+    keys = {
+      {
+        '<leader>do',
+        '<cmd>DiffviewOpen<CR>',
+        desc = '[D]iffview [O]pen',
+      },
+      {
+        '<leader>dc',
+        '<cmd>DiffviewClose<CR>',
+        desc = '[D]iffview [C]lose',
+      },
+    },
+  },
+}

8be218e5425a28c973dec6a9202e4a687598513b
Pawel <23643172+devpawel@users.noreply.github.com>
2025-11-22 10:51:39 +0100

    Remove unneeded indent options

--- init.lua
+++ init.lua
@@ -104,10 +104,6 @@ vim.o.number = true
 --  Experiment for yourself to see if you like it!
 vim.o.relativenumber = true
 
--- Indent with 4 spaces by default
-vim.o.expandtab = true
--- vim.o.shiftwidth = 2 -- Breaks guess-indent.nvim
-
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = 'a'
 

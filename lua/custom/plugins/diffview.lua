334f966e6048ab95742cb1b952abdb893a5ae58f
Pawel <23643172+devpawel@users.noreply.github.com>
2026-07-15 00:26:21 +0200

    Add my changes

---
+++ lua/custom/plugins/diffview.lua
@@ -0,0 +1,6 @@
+vim.pack.add { 'https://github.com/sindrets/diffview.nvim' }
+
+vim.keymap.set('n', '<leader>do', '<cmd>DiffviewOpen<CR>', { desc = '[D]iffview [O]pen' })
+vim.keymap.set('n', '<leader>dc', '<cmd>DiffviewClose<CR>', { desc = '[D]iffview [C]lose' })
+
+require('diffview').setup {}
